129b5afa945536433774a1092c71d80f3f815d02
giovannirwp <giovanni.ribeiro@betrybe.com>
2022-09-17 16:58:24 -0300

    feat: adicionando novos arquivos

--- delivery-full/src/Components/Boxes.js
+++ delivery-full/src/Components/Boxes.js
@@ -7,7 +7,7 @@ function Boxes(props) {
       <div className="box">
         <img src={props.img} alt="" />
         <p>{props.titulo}</p>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href={props.link} target="_blank" rel="noopener noreferrer">
           {props.textoLink}
         </a>
       </div>
--- delivery-full/src/Pages/Home.js
+++ delivery-full/src/Pages/Home.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Boxes from "../Components/Boxes";
 import "./Home.scss";
 import listBoxsNew from "../assets/date";
+const url = "http://localhost:3001/products";
+
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+      console.log(data);
+      setProducts(data)
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="home">
       <h1>Painel de lanches</h1>
       <div className="boxes">
 
-        {listBoxsNew.map((list) => (
+        {products.map((list) => (
           <Boxes
             key={list.id}
             titulo={list.titulo}
             img={list.img}
             textoLink={list.textoLink}
+            link={list.link}
           />
 
         ))}
