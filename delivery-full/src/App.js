43d14dc334ba8f90f50ee77e7b0ca2b6a687053f
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-09-21 18:11:30 -0300

    fix: adicionando nova funcionalidade de busca

--- delivery-full/src/App.js
+++ delivery-full/src/App.js
@@ -6,18 +6,22 @@ import About from "./Pages/About";
 import Contato from "./Pages/Contato";
 import ProductDetails from "./Pages/ProductDetails";
 import NotFound from "./Pages/NotFound";
+import SearchForm from "./Components/SearchForm";
+import Search from "./Pages/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        {/* <SearchForm /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/search" element={<Search />} />
           <Route  path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ delivery-full/src/Pages/Search.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSearchParams, Link } from 'react-router-dom';
+import Boxes from '../Components/Boxes';
+import { useFetch } from '../hooks/useFetch';
+
+export default function Search() {
+  const [searchParams] = useSearchParams();
+
+  const url = "http://localhost:3000/products?" + searchParams;
+
+  const { data: items, loadin, error } = useFetch(url);
+
+  return (
+    <div>
+      <h1>Resultados disponíveis</h1>
+      {items &&
+            items.map((list) => (
+              <Boxes
+                key={list.id}
+                titulo={list.titulo}
+                img={list.img}
+                textoLink={list.textoLink}
+                link={list.link}
+                linkDetalhes={`/products/${list.id}`}
+              />
+            ))}
+    </div>
+  )
+}
