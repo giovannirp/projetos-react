5e781669a9eadecf1c621a3896a1f4c8d41a1fed
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-09-26 18:43:19 -0300

    feat: adicionando estrutura do projeto

---
+++ word-secret/src/components/GameOver.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./GameOver.css";
+
+export default function GameOver() {
+  return (
+    <div>GameOver</div>
+  )
+}
---
+++ word-secret/src/components/StartScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StartScreen.css";
+
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no bota abaixo para começar o jogo</p>
+      <button>Abrir o Jogo</button>
+    </div>
+  );
+}
