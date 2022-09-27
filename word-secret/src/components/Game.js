7b9555277a48d438da47bd001eed9f2c54e07dbd
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-09-27 11:36:46 -0300

    feat: adicionando formulario e css

--- word-secret/src/components/Game.js
+++ word-secret/src/components/Game.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 import "./Game.css";
 
 export default function Game(props) {
   return (
-    <div>
-    <h1>Game</h1>
-    <button onClick={props.verifyLetter}>Finalizar o jogo</button>
-  </div>
-  )
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
+    </div>
+  );
 }
