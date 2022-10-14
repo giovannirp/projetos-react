c375a3cb31670bc72a1fcbd019edcc8e5a7df41a
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-10-14 19:11:14 -0300

    feat: adicionando components

---
+++ todo_typescript_project/src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>React+ ts Todo</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ todo_typescript_project/src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Header = () => {
+  return (
+    <header>
+      <h1>React + TS Todo</h1>
+    </header>
+  );
+};
+
+export default Header;
