a0b85e402df466020b252a74b512fa09eafb623a
Giovanni Pessoal <giovanni_ge@hotmail.com>
2022-10-16 15:12:55 -0300

    feat: criando estrutura de listagem

--- todo_typescript_project/src/Components/TaskList.tsx
+++ todo_typescript_project/src/Components/TaskList.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
+import { ITask } from "../interfaces/Task";
 
-type Props = {}
+import styles from "./TaskList.module.css";
 
-const TaskList = (props: Props) => {
+type Props = {
+  taskList: ITask[];
+};
+
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>Lista da tarefa</p>
-    </div>
-  )
-}
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.taskList}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
+  );
+};
 
-export default TaskList
+export default TaskList;
